fb15f0707677f0d19f9571dcc2e4e2214c07335a
Mateus Santos <mateusap1@gmail.com>
2024-10-04 16:08:23 -0300

    added tests for error scenarios gotosys

--- tests/unit/systems/test_goto_sys.py
+++ tests/unit/systems/test_goto_sys.py
@@ -12,7 +12,7 @@ from simulator.typehints.component_types import (
     GotoPosPayload,
 )
 
-from simulator.utils.Navigation import POI
+from simulator.utils.Navigation import POI, PathNotFound
 
 from unittest.mock import MagicMock
 
@@ -62,6 +62,59 @@ def test_process_event():
     )
     processor.add_path_to_ent.assert_called_with(
         robot, world, HOSPITAL_MAP, (0.0, 0.0), (75.0, 225.0)
+    )
+
+
+def test_process_event_no_target():
+    processor = GotoDESProcessor()
+
+    world = esper.World()
+
+    world.create_entity(HOSPITAL_MAP)
+    robot = world.create_entity(Velocity(x=0.0, y=0.0), Position(x=0.0, y=0.0))
+
+    event_store = simpy.FilterStore(simpy.Environment())
+    event = EVENT(GotoPoiEventTag, GotoPoiPayload(robot, "patientRoom"))
+    event_store.put(event)
+
+    processor.get_event_target = MagicMock(return_value=None)
+    processor.handle_target_error = MagicMock()
+
+    processor.process_event(world, HOSPITAL_MAP, event_store, event)
+
+    processor.get_event_target.assert_called_with(
+        HOSPITAL_MAP, GotoPoiEventTag, GotoPoiPayload(robot, "patientRoom")
+    )
+    processor.handle_target_error.assert_called_with(
+        event_store, GotoPoiPayload(robot, "patientRoom")
+    )
+
+
+def test_process_event_path_not_found():
+    processor = GotoDESProcessor()
+
+    world = esper.World()
+
+    world.create_entity(HOSPITAL_MAP)
+    robot = world.create_entity(Velocity(x=0.0, y=0.0), Position(x=0.0, y=0.0))
+
+    event_store = simpy.FilterStore(simpy.Environment())
+    event = EVENT(GotoPoiEventTag, GotoPoiPayload(robot, "patientRoom"))
+    event_store.put(event)
+
+    path_error = PathNotFound((0.0, 0.0), (75.0, 225.0), None)
+
+    processor.get_event_target = MagicMock(return_value=(75.0, 225.0))
+    processor.add_path_to_ent = MagicMock(side_effect=path_error)
+    processor.handle_path_error = MagicMock()
+
+    processor.process_event(world, HOSPITAL_MAP, event_store, event)
+
+    processor.get_event_target.assert_called_with(
+        HOSPITAL_MAP, GotoPoiEventTag, GotoPoiPayload(robot, "patientRoom")
+    )
+    processor.handle_path_error.assert_called_with(
+        event_store, GotoPoiPayload(robot, "patientRoom"), path_error
     )
 
 
